2e9440cf783199a91ec502666a92d4b4ac564dde
Brian chow <brianchowshunfu@gmail.com>
2022-02-22 07:39:07 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <Router basename='/Brian-GitHub-Page'>
+      <Router>
         {/* <nav>
           <Link to="/home"> Home </Link>
           <Link to="/project"> About </Link>
